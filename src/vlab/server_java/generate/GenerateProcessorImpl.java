e05dd371755a8793c865ac1bf544e3a2f743ab22
Oleg Naumov <oleg.n@imobis.ru>
2022-01-23 20:07:55 +0300

    initial commit

---
+++ src/vlab/server_java/generate/GenerateProcessorImpl.java
@@ -0,0 +1,23 @@
+package vlab.server_java.generate;
+
+import rlcp.generate.GeneratingResult;
+import rlcp.server.processor.generate.GenerateProcessor;
+
+import org.json.JSONObject;
+
+/**
+ * Simple GenerateProcessor implementation. Supposed to be changed as needed to
+ * provide necessary Generate method support.
+ */
+public class GenerateProcessorImpl implements GenerateProcessor {
+    @Override
+    public GeneratingResult generate(String condition) {
+        //do Generate logic here
+        String text = "text";
+        String code = "code";
+        String instructions = "instructions";
+
+
+        return new GeneratingResult(text, code, instructions);
+    }
+}
